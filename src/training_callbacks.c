054b3a4bb7a2d1341f09eb740de6e9d1a6a33a3b
mrsmola <mrsmola@localhost>
2007-01-18 17:36:09 +0000

    included training camp

---
+++ src/training_callbacks.c
@@ -0,0 +1,198 @@
+/*
+   bet_struct.h
+
+   Bygfoot Football Manager -- a small and simple GTK2-based
+   football management game.
+
+   http://bygfoot.sourceforge.net
+
+   Copyright (C) 2005  Gyözö Both (gyboth@bygfoot.com)
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "bygfoot.h"
+#include "game_gui.h"
+#include "support.h"
+#include "training.h"
+#include "training_callbacks.h"
+#include "training_interface.h"
+#include "training_struct.h"
+#include "treeview.h"
+#include "user.h"
+#include "variables.h"
+#include "window.h"
+
+void
+on_b_cancel_clicked                    (GtkButton       *button,
+                                        gpointer         user_data)
+{
+	window_destroy(&window.training_camp);
+}
+
+void
+on_b_ok_clicked                        (GtkButton       *button,
+                                        gpointer         user_data)
+{
+	GtkWidget *rb_camp1;
+    GtkWidget *rb_camp2;
+    GtkHScale *hs_recreation;
+    GtkHScale *hs_training;
+    gdouble value_training;
+	gdouble value_recreation;
+	gint number_camp;
+	Team *current_team = current_user.tm;
+	
+	//Get active radio
+	rb_camp1 = GTK_WIDGET(lookup_widget(window.training_camp, "rb_camp1"));
+    rb_camp2 = GTK_WIDGET(lookup_widget(window.training_camp, "rb_camp2"));
+
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(rb_camp1)))
+		number_camp = 1;
+	else if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(rb_camp2)))
+		number_camp = 2;
+	else
+		number_camp = 3;
+	
+	//Get values for training and recreation	
+    hs_recreation = GTK_HSCALE(lookup_widget(window.training_camp, "hs_recreation"));
+    hs_training = GTK_HSCALE(lookup_widget(window.training_camp, "hs_training"));
+    value_training = gtk_range_get_value(GTK_RANGE(hs_training));
+    value_recreation = gtk_range_get_value(GTK_RANGE(hs_recreation));
+    
+	//Calculate training camp
+	calculateTrainingCamp(current_team, value_training, value_recreation, number_camp);
+
+	//Set new av-values -> GUI
+	game_gui_write_av_skills(current_team);
+	
+	//Set new player values in GUI
+	treeview_show_user_player_list();
+	
+	//Calculate costs of the training camp
+	calculateCostsTrainingCamp(number_camp);
+	
+	//Set new av-values -> GUI
+	game_gui_write_money();
+	
+	window_destroy(&window.training_camp);
+}
+
+void
+on_rb_camp3_clicked                    (GtkButton       *button,
+                                        gpointer         user_data)
+{
+	GtkEntry *tfCosts;
+	gchar buf[SMALL];
+	
+	sprintf(buf, "%d", COSTS_CAMP_3);
+	tfCosts = GTK_ENTRY(lookup_widget(window.training_camp, "tf_costs"));
+    gtk_entry_set_text (tfCosts, buf);
+}
+
+
+void
+on_rb_camp2_clicked                    (GtkButton       *button,
+                                        gpointer         user_data)
+{
+	GtkEntry *tfCosts;
+	gchar buf[SMALL];
+	
+	sprintf(buf, "%d", COSTS_CAMP_2);
+	tfCosts = GTK_ENTRY(lookup_widget(window.training_camp, "tf_costs"));
+    gtk_entry_set_text (tfCosts, buf);
+}
+
+
+void
+on_rb_camp1_clicked                    (GtkButton       *button,
+										gpointer         user_data)
+{
+	GtkEntry *tfCosts;
+	gchar buf[SMALL];
+	
+	sprintf(buf, "%d", COSTS_CAMP_1);
+	tfCosts = GTK_ENTRY(lookup_widget(window.training_camp, "tf_costs"));
+    gtk_entry_set_text (tfCosts, buf);
+}
+
+
+gboolean
+on_hs_recreation_change_value          (GtkRange        *range,
+                                        GtkScrollType    scroll,
+                                        gdouble          value,
+                                        gpointer         user_data)
+{
+	GtkHScale *hs_camp_points;
+	gdouble value_camp_points;
+	gdouble value_recreation;
+	
+	value_recreation = gtk_range_get_value(GTK_RANGE(range));
+	
+	hs_camp_points = GTK_HSCALE(lookup_widget(window.training_camp, "hs_camp_points"));
+	value_camp_points = gtk_range_get_value(GTK_RANGE(hs_camp_points));
+	
+	if ((value > (value_recreation + 0.5)) && (value_camp_points > CAMP_SCALE_MIN))
+	{	
+		gtk_range_set_value(GTK_RANGE(hs_camp_points), value_camp_points - 1.0);
+		return FALSE;
+	}
+	
+	if ((value < (value_recreation - 0.5)) && (value_camp_points < CAMP_SCALE_MAX))
+	{
+		gtk_range_set_value(GTK_RANGE(hs_camp_points), value_camp_points + 1.0);
+		return FALSE;
+	}
+	
+	return TRUE;
+}
+
+
+gboolean
+on_hs_training_change_value            (GtkRange        *range,
+                                        GtkScrollType    scroll,
+                                        gdouble          value,
+                                        gpointer         user_data)
+{
+	GtkHScale *hs_camp_points;
+	gdouble value_camp_points;
+	gdouble value_training;
+	
+	value_training = gtk_range_get_value(GTK_RANGE(range));
+	
+	hs_camp_points = GTK_HSCALE(lookup_widget(window.training_camp, "hs_camp_points"));
+	value_camp_points = gtk_range_get_value(GTK_RANGE(hs_camp_points));
+	
+	if ((value > (value_training + 0.5)) && (value_camp_points > CAMP_SCALE_MIN))
+	{	
+		gtk_range_set_value(GTK_RANGE(hs_camp_points), value_camp_points - 1.0);
+		return FALSE;
+	}
+	
+	if ((value < (value_training - 0.5)) && (value_camp_points < CAMP_SCALE_MAX))
+	{
+		gtk_range_set_value(GTK_RANGE(hs_camp_points), value_camp_points + 1.0);
+		return FALSE;
+	}
+	
+	return TRUE;
+}
+
---
+++ src/training_struct.h
@@ -0,0 +1,39 @@
+/*
+   bet_struct.h
+
+   Bygfoot Football Manager -- a small and simple GTK2-based
+   football management game.
+
+   http://bygfoot.sourceforge.net
+
+   Copyright (C) 2005  Gyözö Both (gyboth@bygfoot.com)
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef TRAINING_STRUCT_H_
+#define TRAINING_STRUCT_H_
+
+#include "bygfoot.h"
+
+#define COSTS_CAMP_1 250000
+#define COSTS_CAMP_2 500000
+#define COSTS_CAMP_3 750000
+
+#define CAMP_SCALE_MIN 0
+#define CAMP_SCALE_MAX 10
+#define CAMP_SCALE_STEP 1
+
+#endif
