66c6bfeff86d72d02d063880e5454a7b6462ebe6
mrsmola <mrsmola@localhost>
2007-01-19 08:44:11 +0000

    included training camp

---
+++ src/training_callbacks.h
@@ -0,0 +1,59 @@
+/*
+   bet_struct.h
+
+   Bygfoot Football Manager -- a small and simple GTK2-based
+   football management game.
+
+   http://bygfoot.sourceforge.net
+
+   Copyright (C) 2005  Gyözö Both (gyboth@bygfoot.com)
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <gtk/gtk.h>
+
+
+void
+on_b_cancel_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_b_ok_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_rb_camp3_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_rb_camp2_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_rb_camp1_clicked                    (GtkButton       *button,
+										gpointer         user_data);
+
+gboolean
+on_hs_recreation_change_value          (GtkRange        *range,
+                                        GtkScrollType    scroll,
+                                        gdouble          value,
+                                        gpointer         user_data);
+
+gboolean
+on_hs_training_change_value            (GtkRange        *range,
+                                        GtkScrollType    scroll,
+                                        gdouble          value,
+                                        gpointer         user_data);
