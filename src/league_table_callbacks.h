b720e469f233696ded4549a4982c0dddd368621b
mrsmola <mrsmola@localhost>
2008-01-29 13:29:38 +0000

    fixed some copy and paste errors

--- src/league_table_callbacks.h
+++ src/league_table_callbacks.h
@@ -1,11 +1,12 @@
 #include <gtk/gtk.h>
 
 
+
 gboolean
-on_window_training_camp_delete_event   (GtkWidget       *widget,
+on_window_league_table_delete_event    (GtkWidget       *widget,
                                         GdkEvent        *event,
                                         gpointer         user_data);
 
 void
-on_b_cancel_clicked                    (GtkButton       *button,
+on_lt_b_ok_clicked                     (GtkButton       *button,
                                         gpointer         user_data);
