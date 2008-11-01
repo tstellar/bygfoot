7b670240abb5d25c446914378cd527a3b60e5a1f
gyboth <gyboth@localhost>
2008-11-01 11:21:28 +0000

    Transfers, star player balking. Ticket price constant adjusted.

--- src/game.c
+++ src/game.c
@@ -1,26 +1,26 @@
 /*
-   game.c
+  game.c
 
-   Bygfoot Football Manager -- a small and simple GTK2-based
-   football management game.
+  Bygfoot Football Manager -- a small and simple GTK2-based
+  football management game.
 
-   http://bygfoot.sourceforge.net
+  http://bygfoot.sourceforge.net
 
-   Copyright (C) 2005  Gyözö Both (gyboth@bygfoot.com)
+  Copyright (C) 2005  Gyözö Both (gyboth@bygfoot.com)
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2
-   of the License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include "cup.h"
@@ -186,7 +186,7 @@ game_get_player(const Team *tm, gint player_type,
 	    if(i < 10)
 		g_print("prob %.3f  ", probs[i]);
 	    g_print("%d %20s health %d cskill %.2f\n", i, player_of_idx_team(tm, i)->name,
-		   player_of_idx_team(tm, i)->health, player_of_idx_team(tm, i)->cskill);
+		    player_of_idx_team(tm, i)->health, player_of_idx_team(tm, i)->cskill);
 	}
 
 	main_exit_program(EXIT_INT_NOT_FOUND, NULL);
@@ -977,8 +977,8 @@ game_post_match(Fixture *fix)
     if((debug > 100 && fixture_user_team_involved(fix) != -1) ||
        debug > 130)
 	g_print("game_post_match: %s - %s\n", 
-	       fix->teams[0]->name,
-	       fix->teams[1]->name);
+		fix->teams[0]->name,
+		fix->teams[1]->name);
 
     if(query_fixture_has_tables(fix))
 	table_update(fix);
