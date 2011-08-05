f6d3aa60e0b0896e050bb06f55e5f51abbccce95
gunnar_g <gunnar_g@localhost>
2011-08-05 11:57:21 +0000

    Continue on windres integration for windows

--- src/bygfoot_private.h
+++ src/bygfoot_private.h
@@ -1,18 +1,15 @@
-/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
-/* DO NOT EDIT ! */
-
 #ifndef BYGFOOT_PRIVATE_H
 #define BYGFOOT_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.1.1.1"
-#define VER_MAJOR	0
-#define VER_MINOR	1
-#define VER_RELEASE	1
-#define VER_BUILD	1
+#define VER_STRING	"2.3.2"
+#define VER_MAJOR	2
+#define VER_MINOR	3
+#define VER_RELEASE	2
+#define VER_BUILD	0
 #define COMPANY_NAME	""
 #define FILE_VERSION	""
-#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define FILE_DESCRIPTION	"Bygfoot"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
